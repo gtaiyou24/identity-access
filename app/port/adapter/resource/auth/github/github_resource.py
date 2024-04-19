e6c69d68fbdb17a9c3c52cf5747501ad7d515270
gtaiyou24 <gtaiyou24@gmail.com>
2024-04-20 05:29:14 +0900

    feat: app/port/adapter/resource/auth/github/github_resource.py

--- app/port/adapter/resource/auth/github/github_resource.py
+++ app/port/adapter/resource/auth/github/github_resource.py
@@ -1,5 +1,3 @@
-import json
-
 import requests
 from di import DIContainer
 from fastapi import APIRouter, Depends
@@ -20,21 +18,22 @@ class GitHubResource:
         self.router.add_api_route("/token", self.token, methods=["POST"], response_model=Token)
 
     def token(self, request: AccessTokenRequest = Depends()) -> Token:
-        application_service = DIContainer.instance().resolve(IdentityApplicationService)
-
-        headers = {'Authorization': f"Bearer {self.__access_token_from(request.code)}"}
-        user = requests.get(f'{self.GITHUB_API}/user', headers=headers).json()
-        emails = requests.get(f'{self.GITHUB_API}/user/emails', headers=headers).json()
-
-        command = AuthenticateProviderUserCommand.github(user, emails)
-        dpo = application_service.authenticate_provider_user(command)
-        return Token.generate(dpo)
-
-    def __access_token_from(self, code: str) -> str:
+        # 一時コード指定で GitHub からアクセストークンを取得する
         access_token = requests.post(
             'https://github.com/login/oauth/access_token',
             headers={'Accept': 'application/json'},
-            params={'client_id': self.__client_id, 'client_secret': self.__client_secret, 'code': code}
+            params={'client_id': self.__client_id, 'client_secret': self.__client_secret, 'code': request.code}
         ).json()
-        return access_token['access_token']
 
+        # アクセストークン指定で GitHub からユーザー情報、メールアドレスを取得する
+        headers = {'Authorization': f"Bearer {access_token['access_token']}"}
+        user = requests.get(f'{self.GITHUB_API}/user', headers=headers).json()
+        emails = requests.get(f'{self.GITHUB_API}/user/emails', headers=headers).json()
+
+        # 認証
+        command = AuthenticateProviderUserCommand.github(user, emails)
+        application_service = DIContainer.instance().resolve(IdentityApplicationService)
+        dpo = application_service.authenticate_provider_user(command)
+
+        # アクセストークンを発行
+        return Token.generate(dpo)
